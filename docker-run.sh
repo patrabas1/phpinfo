43dd4d047212d08ef88dd9f10b3e57ed19ba5aa7
ganimedes-colomar <46420621+ganimedes-colomar@users.noreply.github.com>
2021-09-15 11:08:50 +0200

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -11,15 +11,25 @@ docker image build \
   ./
   
 ## docker container run --entrypoint /bin/sh --interactive --rm --tty local/phpinfo:test
+## find / | grep bin/ | wc -l
+## which php
 
 docker network create phpinfo
 docker container run \
+  --cpus '0.1'
   --detach \
+  --entrypoint /usr/bin/php \
+  --env author=Ganimedes \
+  --label app=phpinfo \
+  --memory 100M \
   --name phpinfo \
   --network phpinfo \
+  --publish 80:8080 \
   --read-only \
   --restart always \
   --user nobody \
   --volume ./src/index.php:/app/index.php:ro \
   --workdir /app/ \
-  local/phpinfo:test
+  local/phpinfo:test \
+  -f /src/index.php \
+  -S 0.0.0.0:8080 \
