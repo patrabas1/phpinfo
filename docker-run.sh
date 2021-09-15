1bd1f64451d5a2398100339305cb240a0d973375
patrabas1 <90602979+patrabas1@users.noreply.github.com>
2021-09-15 11:09:14 +0200

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,24 +1,33 @@
-#!/bin/sh
-
-git clone https://github.com/patrabas1/phpinfo
+git clone https://github.com/ganimedes-colomar/phpinfo
 cd phpinfo
-git checkout 2021-09-Santander
+git checkout 2021-09-santander
 
 docker image build \
   --file ./Dockerfile \
   --no-cache \
-  --tag local\phpinfo:test \
+  --tag local/phpinfo:test \
   ./
   
-docker network create phpinfo
+## docker container run --entrypoint /bin/sh --interactive --rm --tty local/phpinfo:test
+## find / | grep bin/ | wc -l
+## which php
 
+docker network create phpinfo
 docker container run \
+  --cpus '0.1'
   --detach \
+  --entrypoint /usr/bin/php \
+  --env author=Ganimedes \
+  --label app=phpinfo \
+  --memory 100M \
   --name phpinfo \
   --network phpinfo \
+  --publish 80:8080 \
   --read-only \
   --restart always \
   --user nobody \
   --volume ./src/index.php:/app/index.php:ro \
   --workdir /app/ \
-  local/phpinfo:test
+  local/phpinfo:test \
+  -f /src/index.php \
+  -S 0.0.0.0:8080 \
